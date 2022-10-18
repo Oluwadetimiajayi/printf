8158f2984c9fa731f0d6168c81fdb5f674365185
Ebube Ekeocha <maceyicon@gmail.com>
2022-10-18 11:14:46 -0700

    New changes

--- _printf.c
+++ _printf.c
@@ -2,43 +2,54 @@
 
 /**
  * _printf - prints formatted output
- * @format: initial string with no link to corresponding specifier
+ * @format: input string
  * @...: varied list of args
  * Return: number of chars printed
  */
 
 int _printf(const char *format, ...)
 {
-	int i, j;
-	int count = 0;
-	va_list ap;
-	intermediary ids[] = { {'c', _print_char}, {'s', _print_string}, {'i', _print_int}, {'d', _print_int}, {'%', _print_mod}, {'\0', NULL} };
+	unsigned int i = 0, len = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
 
-	va_start (ap, format);
-	for (i = 0; format[i]; i++)
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
+	{
 		if (format[i] == '%')
-		{
-			i++
-			for (; format[i] != '\0'; i++)
-			{
-				for (j = 0; ids[j].id != '\0'; j++)
-					if (format[i] == ids[j].id)
-					{
-						count += ids[j].f(ap);
-						break;
-					}
-				if (ids[j].id)
-					break;
-			}
-			if (format[i] == '\0')
-				return (-1);
-		}
-		else
-		{
-			write(1, &format[i], 1);
-			count += 1;
-		}
 
-	va_end(ap);
-	return (count);
+			{
+				if (format[i + 1] == '\0')
+				{
+					print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+					return (-1);
+				}
+				else
+				{
+					function = get_print_func(format, i + 1);
+					if (function == NULL)
+					{
+						if (format[i + 1] == ' ' && !format[i + 2])
+							return (-1);
+						handl_buf(buffer, format[i], ibuf), len++, i--;
+					}
+					else
+					{
+						len += function(arguments, buffer, ibuf);
+						i += ev_print_func(format, i + 1);
+					}
+				} i++;
+			}
+		else
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
+	}
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (len);
 }
--- main.h
+++ main.h
@@ -11,17 +11,18 @@
 #include <ctype.h>
 
 /**
- * struct intermediary - Struct token
+ * struct vault - Struct token
+ * Description: stores pointers to called functions
  * @id - identifies the type of argument
  * @format: format specifier
- * @f: pointer to function to call
+ * @f: pointer to called functions
  */
 
-typedef struct intermediary
+typedef struct vault
 {
-	char id;
-	int (*f)(va_list list);
-} intermediary;
+	char *id;
+	int (*f)(va_list, char *, unsigned int);
+} vault_t;
 
 int _printf(const char *format, ...);
 int _print_mod(va_list ls);
@@ -79,5 +80,43 @@ int octal_func(va_list list)
 
 	return (a);
 }
+
+int print_prg(va_list __attribute__((unused)), char *, unsigned int);
+int print_chr(va_list arguments, char *buf, unsigned int ibuf);
+int print_str(va_list arguments, char *buf, unsigned int ibuf);
+int print_int(va_list arguments, char *buf, unsigned int ibuf);
+int print_bnr(va_list arguments, char *buf, unsigned int ibuf);
+int print_unt(va_list arguments, char *buf, unsigned int ibuf);
+int print_oct(va_list arguments, char *buf, unsigned int ibuf);
+int print_hex(va_list arguments, char *buf, unsigned int ibuf);
+int print_upx(va_list arguments, char *buf, unsigned int ibuf);
+int print_usr(va_list arguments, char *buf, unsigned int ibuf);
+int print_add(va_list arguments, char *buf, unsigned int ibuf);
+int print_rev(va_list arguments, char *buf, unsigned int ibuf);
+int print_rot(va_list arguments, char *buf, unsigned int ibuf);
+int prinlint(va_list arguments, char *buf, unsigned int ibuf);
+int prinlunt(va_list arguments, char *buf, unsigned int ibuf);
+int prinloct(va_list arguments, char *buf, unsigned int ibuf);
+int prinlhex(va_list arguments, char *buf, unsigned int ibuf);
+int prinlupx(va_list arguments, char *buf, unsigned int ibuf);
+int prinhint(va_list arguments, char *buf, unsigned int ibuf);
+int prinhunt(va_list arguments, char *buf, unsigned int ibuf);
+int prinhoct(va_list arguments, char *buf, unsigned int ibuf);
+int prinhhex(va_list arguments, char *buf, unsigned int ibuf);
+int prinhupx(va_list arguments, char *buf, unsigned int ibuf);
+int prinpint(va_list arguments, char *buf, unsigned int ibuf);
+int prinnoct(va_list arguments, char *buf, unsigned int ibuf);
+int prinnhex(va_list arguments, char *buf, unsigned int ibuf);
+int prinnupx(va_list arguments, char *buf, unsigned int ibuf);
+int prinsint(va_list arguments, char *buf, unsigned int ibuf);
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+int ev_print_func(const char *s, int index);
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
+int print_buf(char *buf, unsigned int nbuf);
+char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
+char *fill_oct_array(char *bnr, char *oct);
+char *fill_long_oct_array(char *bnr, char *oct);
+char *fill_short_oct_array(char *bnr, char *oct);
+char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
 
 #endif
