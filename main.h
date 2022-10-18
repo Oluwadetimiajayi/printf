dc510b400d6b222b90efcc1b6ef693d3d36b0411
Ebube Ekeocha <maceyicon@gmail.com>
2022-10-18 16:53:20 -0700

    Try again

--- main.h
+++ main.h
@@ -25,62 +25,6 @@ typedef struct vault
 } vault_t;
 
 int _printf(const char *format, ...);
-int _print_mod(va_list ls);
-int _print_string(va_list ls);
-int _print_int(va_list ls);
-int width_func(va_list list);
-int _putchar(char c);
-int char_func(va_list list);
-int print_char(va_list list);
-int str_func(va_list list);
-int stringupperCase_func(va_list list);
-int rev_func(va_list list);
-int rot13_func(va_list list);
-
-int binary_func(va_list list)
-{
-	int b = va_arg(list, unsigned int);
-	int a = convert(b, 2, 1, 0);
-
-	return (a);
-}
-
-int unsig_int_func(va_list list)
-{
-	unsigned int n = va_arg(list, unsigned int);
-
-	if (n > UINT_MAX)
-		n = n - UINT_MAX - 1;
-
-	print_number(n);
-	return (0);
-}
-
-
-int hex_upper_func(va_list list)
-{
-	int num = va_arg(list, unsigned int);
-	int a = convert(num, 16, 1, 0);
-
-	return (a);
-}
-
-int hex_lower_func(va_list list)
-{
-	int num = va_arg(list, unsigned int);
-	int a = convert(num, 16, 0, 0);
-
-	return (a);
-}
-
-int octal_func(va_list list)
-{
-	int num = va_arg(list, unsigned int);
-	int a = convert(num, 8, 0, 0);
-
-	return (a);
-}
-
 int print_prg(va_list __attribute__((unused)), char *, unsigned int);
 int print_chr(va_list arguments, char *buf, unsigned int ibuf);
 int print_str(va_list arguments, char *buf, unsigned int ibuf);
