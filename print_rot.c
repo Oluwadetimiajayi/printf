9bea7605bad96a2f7bf816de9c660c2df82a1590
Oluwadetimiajayi <oluwadetimiajayi@gmail.com>
2022-10-18 19:58:35 -0700

    good fro

--- print_rot.c
+++ print_rot.c
@@ -4,7 +4,7 @@
  * print_rot - writes the str in ROT13
  * @arguments: input string
  * @buf: buffer pointer
- * @ibuf: index for buffer pointer
+ * @ibuf: index for buffer pointer.
  * Return: number of chars printed.
  */
 
