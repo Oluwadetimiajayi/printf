5a9777522a46443e89061bf769bccfd8749004c6
Oluwadetimiajayi <oluwadetimiajayi@gmail.com>
2022-10-18 19:38:56 -0700

    correct

--- print_usr.c
+++ print_usr.c
@@ -3,9 +3,9 @@
  * print_usr - prints a string and values of
  * non-printed chars
  * @arguments: input string
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: number of chars printed
+ * @buf: buffer pointer.
+ * @ibuf: index for buffer pointer.
+ * Return: number of chars printed.
  */
 int print_usr(va_list arguments, char *buf, unsigned int ibuf)
 {
