950a118a3f62627769115cbc06a15471a49e4dae
Oluwadetimiajayi <oluwadetimiajayi@gmail.com>
2022-10-18 19:17:49 -0700

    adjusted

--- fill_hex_array.c
+++ fill_hex_array.c
@@ -2,8 +2,8 @@
 /**
  * fill_hex_array - writes the character c to stdout
  *
- * @bnr: array where is stored the binary.
- * @hex: array where is stored the hexadecimal.
+ * @bnr: array where the binary is stored the binary.
+ * @hex: array where the hexadecimal is stored the hexadecimal.
  * @isupp: integer that determines if the hexadecimal array is
  * in uppercase or lowercase letter.
  * @limit: size of hex
