7694e272614a60cf6db212c06406208289378815
Ebube Ekeocha <maceyicon@gmail.com>
2022-10-18 18:32:02 -0700

    corrections

--- _printf.c
+++ _printf.c
@@ -27,11 +27,11 @@ int _printf(const char *format, ...)
 				if (format[i + 1] == '\0')
 				{
 					print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-					return (-1);
+					return (-1)
+					function = get_print_func(format, i + 1);
 				}
 				else
 				{
-					function = get_print_func(format, i + 1);
 					if (function == NULL)
 					{
 						if (format[i + 1] == ' ' && !format[i + 2])
@@ -45,10 +45,10 @@ int _printf(const char *format, ...)
 					}
 				} i++;
 			}
-		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
+			else
+				handl_buf(buffer, format[i], ibuf), len++;
+			for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+				;
 	}
 	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
 	return (len);
