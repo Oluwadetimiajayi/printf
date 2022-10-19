d2afc4a196035f9b86e191289cfafeb8e2ce409b
Oluwadetimiajayi <oluwadetimiajayi@gmail.com>
2022-10-18 19:22:54 -0700

    more adjustments

--- fill_long_oct_array.c
+++ fill_long_oct_array.c
@@ -2,8 +2,8 @@
 /**
  * fill_long_oct_array - calculates a long octal number
  *
- * @bnr: array wherethe binary is stored the binary.
- * @oct: array where the octal is stored the octal.
+ * @bnr: array wherethe binary is stored.
+ * @oct: array where the octal is stored.
  *
  * Return: binary array.
  */
